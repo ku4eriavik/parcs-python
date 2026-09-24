8038ae0d8086e67520404aba7904a9ef5388c431
Andrii Vasyliev <43913782+ku4eriavik@users.noreply.github.com>
2026-09-24 23:14:26 +0000

    [Refactor]: deleted unused and obsolete entities

--- ubuntu_install.sh
+++
@@ -1,5 +0,0 @@
-# sudo apt-get -y update
-# sudo apt-get install -y git vim python-dev libxml2-dev libxslt-dev
-virtualenv -p python2 py2-env
-source activate py2-env/bin/activate
-pip install netifaces pyro4 py-cpuinfo flask requests
